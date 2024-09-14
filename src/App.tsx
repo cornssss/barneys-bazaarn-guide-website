dbeedfe31013530121733995e50e19f8069629ec
“Jerald <dlarejcasulla12@gmail.com>
2024-09-15 01:31:34 +0800

    1.20

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,35 @@
-// import { useEffect, useState } from "react";
-// import "./App.css";
-// import { CategoryType, PricesType } from "./types";
-// import Table from "./Table";
-
-// const api =
-//   "https://pixels-server.pixels.xyz/cache/marketplace/listings/count?v=";
-
-// function App() {
-//   const [categories, setCategories] = useState<CategoryType[] | null>(null);
-//   const [market, setMarket] = useState(null);
-
-//   useEffect(() => {
-//     // Fetch categories and prices simultaneously
-//     const fetchCategories = fetch("/categories.json")
-//       .then((response) => response.json())
-//       .catch((error) => console.error("Error fetching categories:", error));
-
-//     const fetchDynamicPrices = fetch(api + Math.floor(Date.now() / 1000))
-//       .then((response) => response.json())
-//       .catch((error) => console.error("Error fetching dynamic prices:", error));
-
-//     Promise.all([fetchCategories, fetchDynamicPrices])
-//       .then(([categoriesData, marketData]) => {
-//         setCategories(categoriesData);
-//         setMarket(marketData);
-//       })
-//       .catch((error) => console.error("Error fetching data:", error));
-//   }, []);
-
-//   if (categories === null || market === null)
-//     return (
-//       <div className="container">
-//         <p>Loading categories...</p>
-//       </div>
-//     );
-
-//   return (
-//     <div className="container">
-//       <h1>Barney's Bazaarn Crafting Recipes v2.1</h1>
-//       <p>Reload the page to update the prices. Enjoy!</p>
-//       <p>
-//         Contact me on discord for feedbacks and suggestions :){" "}
-//         <span style={{ fontWeight: "bold", fontSize: 20 }}>mira.gg</span>
-//       </p>
-//       <Table craftings={categories} market={market} />
-//     </div>
-//   );
-// }
-
-// export default App;
 import { useEffect, useState } from "react";
 import "./App.css";
 import { CategoryType, PricesType } from "./types";
 import Table from "./Table";
 
+const api =
+  "https://pixels-server.pixels.xyz/cache/marketplace/listings/count?v=";
+
 function App() {
   const [categories, setCategories] = useState<CategoryType[] | null>(null);
-  const [prices, setPrices] = useState<PricesType | null>(null);
+  const [market, setMarket] = useState(null);
 
   useEffect(() => {
-    fetch("/categories.json")
+    // Fetch categories and prices simultaneously
+    const fetchCategories = fetch("/categories.json")
       .then((response) => response.json())
-      .then((categories) => setCategories(categories))
       .catch((error) => console.error("Error fetching categories:", error));
 
-    fetch("/prices.json")
+    // const fetchDynamicPrices = fetch(api + Math.floor(Date.now() / 1000))/
+    const fetchDynamicPrices = fetch("/market.json")
       .then((response) => response.json())
-      .then((prices) => setPrices(prices))
-      .catch((error) => console.error("Error fetching prices:", error));
+      .catch((error) => console.error("Error fetching dynamic prices:", error));
+
+    Promise.all([fetchCategories, fetchDynamicPrices])
+      .then(([categoriesData, marketData]) => {
+        setCategories(categoriesData);
+        setMarket(marketData);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  if (categories === null || prices === null)
+  if (categories === null || market === null)
     return (
       <div className="container">
         <p>Loading categories...</p>
@@ -79,16 +38,16 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Barney's Bazaarn Crafting Recipes v1.17</h1>
+      <h1>Barney's Bazaarn Crafting Recipes v1.20</h1>
       <p>
-        Prices are manually updated every hour from a secured API, as there are
-        no public endpoints available yet. Please pixels :(
+        Prices are manually updated, as there are no public endpoints available
+        yet. Please pixels :(
       </p>
       <p>
         Contact me on discord for feedbacks and suggestions :){" "}
         <span style={{ fontWeight: "bold", fontSize: 20 }}>mira.gg</span>
       </p>
-      <Table categories_={categories} prices_={prices} />
+      <Table craftings={categories} market={market} />
     </div>
   );
 }
--- src/Table.tsx
+++ src/Table.tsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from "react";
 import { CategoryType, PricesType, TierType } from "./types";
 
 interface TableProps {
-  categories_: CategoryType[];
-  prices_: PricesType;
+  craftings: CategoryType[];
+  market: any;
 }
 
-const Table: React.FC<TableProps> = ({ categories_, prices_ }) => {
-  const [categories, setCategories] = useState<CategoryType[]>(categories_);
-  const [prices, setPrices] = useState<PricesType>(prices_);
+const Table: React.FC<TableProps> = ({ craftings, market }) => {
+  const [categories, setCategories] = useState<CategoryType[]>(craftings);
+  //   const [prices, setPrices] = useState<PricesType>(prices_);
 
   useEffect(() => {
     calculatePrices();
@@ -30,7 +30,7 @@ const Table: React.FC<TableProps> = ({ categories_, prices_ }) => {
 
         tier.Items.forEach((item) => {
           item.totalQuantity = tier.Multiplier * item.Quantity;
-          item.Price = priceMapping[item.id] ?? prices?.[item.id]?.coins ?? 0;
+          item.Price = priceMapping[item.id] ?? market?.prices?.[item.id] ?? 0;
           item.totalPrice = item.Price * item.totalQuantity;
           tierPrice += item.Price * item.Quantity;
         });
@@ -105,6 +105,7 @@ const Table: React.FC<TableProps> = ({ categories_, prices_ }) => {
                       <th>Name</th>
                       <th>Current Price</th>
                       <th>Total Quantity</th>
+                      {/* <th>Market Quantity</th> */}
                       <th>Total Price</th>
                     </tr>
                   </thead>
@@ -123,6 +124,7 @@ const Table: React.FC<TableProps> = ({ categories_, prices_ }) => {
                         <td>{item.Name}</td>
                         <td>{item.Price}</td>
                         <td>{item.totalQuantity}</td>
+                        {/* <td>{market?.counts?.[item.id]}</td> */}
                         <td>{item.totalPrice}</td>
                       </tr>
                     ))}
@@ -148,155 +150,3 @@ const Table: React.FC<TableProps> = ({ categories_, prices_ }) => {
 };
 
 export default Table;
-// import React, { useState, useEffect } from "react";
-// import { CategoryType, PricesType, TierType } from "./types";
-
-// interface TableProps {
-//   craftings: CategoryType[];
-//   market: any;
-// }
-
-// const Table: React.FC<TableProps> = ({ craftings, market }) => {
-//   const [categories, setCategories] = useState<CategoryType[]>(craftings);
-//   //   const [prices, setPrices] = useState<PricesType>(prices_);
-
-//   useEffect(() => {
-//     calculatePrices();
-//   }, []);
-
-//   const calculatePrices = () => {
-//     const categoriesTemp = [...categories];
-
-//     categoriesTemp.forEach((category) => {
-//       category.Tiers.forEach((tier) => {
-//         let tierPrice = 0;
-
-//         const priceMapping: { [key: string]: number } = {
-//           itm_basebox: 400,
-//           itm_tier1box: category.Tiers[0].Price,
-//           itm_tier2box: category.Tiers[1].Price,
-//           itm_tier3box: category.Tiers[2].Price,
-//         };
-
-//         tier.Items.forEach((item) => {
-//           item.totalQuantity = tier.Multiplier * item.Quantity;
-//           item.Price = priceMapping[item.id] ?? market?.prices?.[item.id] ?? 0;
-//           item.totalPrice = item.Price * item.totalQuantity;
-//           tierPrice += item.Price * item.Quantity;
-//         });
-
-//         tier.Price = tierPrice;
-//       });
-//     });
-
-//     setCategories(categoriesTemp);
-//   };
-
-//   const handleMultiplierChange = (
-//     category: CategoryType,
-//     tier: TierType,
-//     value: string
-//   ) => {
-//     const multiplier = Number(value);
-
-//     if (!Number.isInteger(multiplier) || multiplier <= 0) {
-//       console.error(
-//         "Invalid input: Please enter a whole number greater than 0."
-//       );
-//       return;
-//     }
-
-//     tier.Multiplier = multiplier;
-
-//     calculatePrices();
-//   };
-
-//   return (
-//     <div>
-//       {categories.map((category, index) => (
-//         <div key={index}>
-//           {category.Tiers.map((tier, tierIndex) => {
-//             const borderColor = category.Color;
-
-//             return (
-//               <div key={tierIndex}>
-//                 <div className="multiplier-container">
-//                   <label className="multiplier-label">
-//                     <span style={{ fontWeight: "bold" }}>
-//                       {category.Name} T{tier.Tier} - {tier.Points} Points
-//                     </span>
-//                     {" ------------ Multiplier: "}
-//                   </label>
-//                   <input
-//                     type="number"
-//                     value={tier.Multiplier}
-//                     min="1"
-//                     onChange={(e) => {
-//                       handleMultiplierChange(category, tier, e.target.value);
-//                     }}
-//                     className="multiplier-input"
-//                   />
-//                 </div>
-
-//                 <table
-//                   style={{
-//                     border: `2px solid ${borderColor}`,
-//                     borderCollapse: "collapse",
-//                   }}
-//                 >
-//                   <thead
-//                     style={{
-//                       background: borderColor,
-//                       borderCollapse: "collapse",
-//                     }}
-//                   >
-//                     <tr>
-//                       <th></th>
-//                       <th>Name</th>
-//                       <th>Current Price</th>
-//                       <th>Total Quantity</th>
-//                       {/* <th>Market Quantity</th> */}
-//                       <th>Total Price</th>
-//                     </tr>
-//                   </thead>
-//                   <tbody>
-//                     {tier.Items.map((item, itemIndex) => (
-//                       <tr key={itemIndex}>
-//                         <td>
-//                           <img
-//                             src={item.Icon}
-//                             style={{
-//                               width: 40,
-//                               height: 40,
-//                             }}
-//                           />
-//                         </td>
-//                         <td>{item.Name}</td>
-//                         <td>{item.Price}</td>
-//                         <td>{item.totalQuantity}</td>
-//                         {/* <td>{market?.counts?.[item.id]}</td> */}
-//                         <td>{item.totalPrice}</td>
-//                       </tr>
-//                     ))}
-//                     <tr>
-//                       <th colSpan={3}> </th>
-//                       <th>Overall Price</th>
-//                       <th>{tier.Price * tier.Multiplier}</th>
-//                     </tr>
-//                     <tr>
-//                       <th colSpan={3}> </th>
-//                       <th colSpan={1}>Tier Price / Points</th>
-//                       <th>{tier.Price / tier.Points}</th>
-//                     </tr>
-//                   </tbody>
-//                 </table>
-//               </div>
-//             );
-//           })}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Table;
